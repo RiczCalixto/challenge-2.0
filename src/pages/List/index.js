fe37d15a482330e44a59d7c0a02450a22049cce3
Ricardo Calixto <rcalixtos@gmail.com>
2019-09-01 09:25:08 -0300

    Query do graphQL para consumir os dados da API. Buscar os primeiros 50 pokemons.

--- src/pages/List/index.js
+++ src/pages/List/index.js
@@ -1,6 +1,28 @@
 import React from 'react';
+import { gql } from 'apollo-boost';
+import { useQuery } from '@apollo/react-hooks';
+
+const GET_POKEMONS_LIST = gql`
+  query {
+    pokemons(first: 50) {
+      image
+      number
+      name
+    }
+  }
+`;
 
 const List = () => {
+  const { loading, error, data } = useQuery(GET_POKEMONS_LIST);
+  console.log(data);
+  console.log(data.pokemons);
+
+  const { pokemons } = data;
+  console.log(pokemons);
+
+  if (loading) return <h1>Carregando ...</h1>;
+  if (error) return <h1>Erro ... =(</h1>;
+
   return <>Hello from list !</>;
 };
 
